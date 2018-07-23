bdeae8febfe429cb7e2a70f742789291df9d9f3c
RichFelton <rfelton.math@gmail.com>
2018-07-22 22:22:40 -0400

    first commit

---
+++ DatabaseFirstPractice/Models/Rental.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseFirstPractice.Models
+{
+    public class Rental
+    {
+        [Key]
+        public int RentalID { get; set; }
+        public DateTime RentalDate { get; set; }
+
+        [ForeignKey("Movie")]
+        public int MovieID { get; set; }
+        public virtual Movie Movie { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
